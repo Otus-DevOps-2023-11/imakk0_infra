bb45d0a89858c5861e8b0b02352be039133a86c7
imakk0 <dragonetwork@yandex.ru>
2023-12-25 00:22:40 +0300

    add permissions +x to bash scripts

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 sudo apt install git -y
 echo "------GIT CLONE REDDIT REPO------"
 cd ~/ && git clone -b monolith https://github.com/express42/reddit.git
-chmod 655 ~/reddit/*
 echo "------CHECK RERDDIT REPO EXIST------"
 cd ~/reddit && ls
 echo "------BUNDLE INSTALL------"
